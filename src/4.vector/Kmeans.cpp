24365ff99981a830c86fcd49201cf8b99d481db2
JorgePoblete <jpobleteriquelme@gmail.com>
2015-11-09 12:15:53 -0300

    upload

---
+++ src/4.vector/Kmeans.cpp
@@ -0,0 +1,90 @@
+#include "Kmeans.hpp"
+
+Kmeans::Kmeans(double *points, double *centroids, int t, int k, int n, int d)
+{
+	Utils utils;
+	membership = (int *) malloc (sizeof(int)*n);
+	counter = (int *) malloc (sizeof(int)*k);
+	new_centroids = (double *) _mm_malloc (sizeof(double)*k*d,64);
+
+	int changed;
+	do
+	{
+		utils.start_timer("iteration");
+		//reinitialization of variables
+		for (int i=0; i<k; i++)
+		{
+			for (int j=0; j<d; j++)
+				new_centroids[i*d+j] = 0.0;
+			counter[i] = 0;
+		}
+
+		utils.start_timer("estep");
+		changed = estep(points, centroids, k, n, d);
+		utils.stop_timer("estep");
+
+		utils.start_timer("mstep");
+		mstep(points, centroids, k, n, d);
+		utils.stop_timer("mstep");
+
+		utils.stop_timer("iteration");
+		t--;
+	} while (t > 0 && changed > 0);
+	std::cout << "Tiempo estep promedio: " << utils.get_prom_time("estep") << std::endl;
+	std::cout << "Tiempo estep total: " << utils.get_total_time("estep") << std::endl;
+	std::cout << "Tiempo mstep promedio: " << utils.get_prom_time("mstep") << std::endl;
+	std::cout << "Tiempo mstep total: " << utils.get_total_time("mstep") << std::endl;
+	std::cout << "Tiempo iteracion promedio: " << utils.get_prom_time("iteration") << std::endl;
+	std::cout << "Tiempo iteracion total: " << utils.get_total_time("iteration") << std::endl;
+}
+
+int Kmeans::estep(double *points, double *centroids, int k, int n, int d)
+{
+	int changed = 0, kmin;
+	double dmin,dist;
+	__assume_aligned(points, 64);
+	__assume_aligned(centroids, 64);
+	for (int x=0; x<n; x++)
+	{
+		dmin = 99e99;
+		kmin = 0;
+		for (int c=0; c<k; c++)
+		{
+			//euclid distance on a d-dimensional space
+			dist = 0.0;
+			for (int m=0; m<d; m++)
+			{
+				dist += (points[x*d+m]-centroids[c*d+m]) * (points[x*d+m]-centroids[c*d+m]);
+			}
+			dist = sqrt(dist);
+			if (dmin > dist)
+			{
+				dmin = dist;
+				kmin = c;
+			}
+		}
+		if (membership[x] != kmin)
+			changed = changed + 1;
+		membership[x] = kmin;
+	}
+	return changed;
+}
+void Kmeans::mstep(double *points, double *centroids, int k, int n, int d)
+{
+	__assume_aligned(points, 64);
+	__assume_aligned(centroids, 64);
+	__assume_aligned(new_centroids, 64);
+	for (int x=0; x<n; x++)
+	{
+		int c = membership[x];
+		counter[c]++;
+		for (int m=0; m<d; m++)
+			new_centroids[c*d+m] += points[x*d+m];
+	}
+	for (int c=0; c<k; c++)
+	{
+		if (counter[c] > 0)
+			for (int m=0; m<d; m++)
+				centroids[c*d+m] = new_centroids[c*d+m] / counter[c];
+	}
+}
---
+++ src/4.vector/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include "Kmeans.hpp"
+#include "utils.hpp"
+
+int main(int argc, char** argv)
+{
+    std::cout.precision(10);
+    if (argc < 5){
+        exit (0);
+    }
+    int k = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    int d = atoi(argv[3]);
+    int t = atoi(argv[4]);
+    //asignacion de memoria
+    double * data = (double *) _mm_malloc (sizeof(double)*n*d,64);
+    double * means = (double *) _mm_malloc (sizeof(double)*k*d,64);
+    //lectura de datos
+    for (int i=0; i<n;i++)
+    {
+        for (int j=0; j<d; j++)
+            std::cin >> data[i*d+j];
+    }
+    //se asignan los primeros k datos
+    for (int i=0; i<k; i++)
+    {
+        for (int j=0; j<d;j++)
+        {
+            means[i*d+j] = data[i*d+j];
+        }
+    }
+    Utils utils;
+    utils.start_timer("kmeans");
+    Kmeans kmeans(data,means,t,k,n,d);
+    utils.stop_timer("kmeans");
+    std::cout << "Tiempo kmeans total: " << utils.get_total_time("kmeans") << std::endl;
+
+/*
+    for (int i=0; i<k;i++)
+    {
+        for (int j=0; j<d; j++)
+            std::cout << means[i*d+j] << " ";
+        std::cout << std::endl;
+    }
+*/
+    return 0;
+}
